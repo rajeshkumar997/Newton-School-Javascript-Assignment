4fbd1f43bcf715925a470c6f52b7b59dadcc67f3
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-07-20 20:46:57 +0530

    Create index.js

---
+++ JS Getters & Setters, JS Date, JS Errors, Prototype Inheritance, Intermediate fu ... - In Class/Hot or Cold: Part-1/index.js
@@ -0,0 +1,9 @@
+// function displayRandomNumber() {
+//     //let x = Math.floor(Math.random() * (20 - (-20) + 1)) + (-20);
+//     document.getElementById("num").innerHTML = x;
+
+// }
+function displayRandomNumber() {
+    let x = Math.floor((Math.random() * 40) - 20);
+    document.getElementById("num").innerHTML = x;
+}
